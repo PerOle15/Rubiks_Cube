b8d63b272470ffd4499df5c37ade54442e754af4
PerOle15 <ole.pechmann@gmail.com>
2024-04-19 23:37:40 +0200

    Coloring faces working

--- readme.md
+++ readme.md
@@ -1,14 +1,20 @@
-# Three.js Journey
+# Rubik's Cube Simulation
+
+## Description
+
+Aiming to simulate a Rubik's Cube and solve it. (Solver isn't implemented yet)
+See demo here: [Link](https://rubiks-cube-iota.vercel.app/)
 
 ## Setup
-Download [Node.js](https://nodejs.org/en/download/).
-Run this followed commands:
 
-``` bash
-# Install dependencies (only the first time)
+Download [Node.js](https://nodejs.org/en/download/).
+Run the following commands:
+
+```bash
+# Install dependencies
 npm install
 
-# Run the local server at localhost:8080
+# Run the local server at localhost:5173
 npm run dev
 
 # Build for production in the dist/ directory
--- src/Experience/World/Cube/Cube.js
+++ src/Experience/World/Cube/Cube.js
@@ -10,6 +10,14 @@ export default class Cube {
     this.cubeDim = 0.5
     this.cubeSpacing = 0.02
 
+    this.edgeSegments = 5
+
+    // Indexes for coloring of the cube faces
+    this.rightFaceIndex =
+      2 * (2 * Math.pow(this.edgeSegments, 2) + 2 * this.edgeSegments)
+    this.deltaIndex =
+      2 * (4 * Math.pow(this.edgeSegments, 2) + 4 * this.edgeSegments) + 2
+
     this.setGeometry()
     this.setMaterial()
     this.setMeshes()
@@ -20,15 +28,79 @@ export default class Cube {
       this.cubeDim,
       this.cubeDim,
       this.cubeDim,
-      5,
+      this.edgeSegments,
       0.04
     )
   }
 
   setMaterial() {
     this.material = new THREE.MeshStandardMaterial({
-      color: 0xad286b,
+      vertexColors: true,
     })
+
+    this.positionAttribute = this.geometry.getAttribute('position')
+
+    this.colorsArray = new Float32Array(this.positionAttribute.count * 3)
+  }
+
+  setFaceColor(geometry, side) {
+    // Reset geometry to black
+    for (let i = 0; i < 6; i++) {
+      const faceIndex = this.rightFaceIndex + this.deltaIndex * i
+      this.setFaceArray(faceIndex, new THREE.Color(0, 0, 0))
+      this.setFaceArray(faceIndex + 1, new THREE.Color(0, 0, 0))
+    }
+
+    let index
+    let color
+    switch (side) {
+      case 'right':
+        index = this.rightFaceIndex
+        color = new THREE.Color('red')
+        break
+      case 'left':
+        index = this.rightFaceIndex + this.deltaIndex
+        color = new THREE.Color('orange')
+        break
+      case 'top':
+        index = this.rightFaceIndex + this.deltaIndex * 2
+        color = new THREE.Color('yellow')
+        break
+      case 'bottom':
+        index = this.rightFaceIndex + this.deltaIndex * 3
+        color = new THREE.Color('white')
+        break
+      case 'front':
+        index = this.rightFaceIndex + this.deltaIndex * 4
+        color = new THREE.Color('blue')
+        break
+      case 'back':
+        index = this.rightFaceIndex + this.deltaIndex * 5
+        color = new THREE.Color('green')
+        break
+
+      default:
+        break
+    }
+    this.setFaceArray(index, color)
+    this.setFaceArray(index + 1, color)
+
+    geometry.setAttribute(
+      'color',
+      new THREE.Float32BufferAttribute(this.colorsArray, 3)
+    )
+  }
+
+  setFaceArray(index, color) {
+    this.colorsArray[9 * index + 0] = color.r
+    this.colorsArray[9 * index + 1] = color.g
+    this.colorsArray[9 * index + 2] = color.b
+    this.colorsArray[9 * index + 3] = color.r
+    this.colorsArray[9 * index + 4] = color.g
+    this.colorsArray[9 * index + 5] = color.b
+    this.colorsArray[9 * index + 6] = color.r
+    this.colorsArray[9 * index + 7] = color.g
+    this.colorsArray[9 * index + 8] = color.b
   }
 
   setMeshes() {
@@ -56,5 +128,7 @@ export default class Cube {
     // // Get bounding Coordinates for group
     // const groupBox = new THREE.Box3().setFromObject(this.cubeGroup)
     // console.log(groupBox)
+
+    this.setFaceColor(this.smallCubes[0].geometry, 'top')
   }
 }
